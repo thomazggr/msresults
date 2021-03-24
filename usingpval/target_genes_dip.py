8e3dbf6dd3021c0a7479fe070bc03e6523ece97c
Thomaz Guadagnini <thomaz@vivaldi.net>
2021-03-24 15:42:39 -0300

    nash separado

--- usingpval/target_genes_dip.py
+++ usingpval/target_genes_dip.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-genes = pd.read_csv('./usingpval/mirdip/mirdip_results.csv', sep=',', skiprows=27)
-genes.drop(columns=['Pseudogene', 'Score Class'], inplace=True)
-colunas = ['gene', 'uniprot', 'mir', 'score', 'nsources', 'sources']
+genes = pd.read_csv("mirdip_nash.csv", sep=",", skiprows=28)
+genes.drop(columns=["Pseudogene", "Score Class"], inplace=True)
+colunas = ["gene", "uniprot", "mir", "score", "nsources", "sources"]
 genes.columns = colunas
-genes_sources = genes.query('nsources >= 14')
+genes_sources = genes.query("nsources >= 14")
 
-a = pd.DataFrame(genes_sources['gene'].unique())
-a.columns = ['genes']
-a['genes'].to_clipboard(index=False)
+a = pd.DataFrame(genes_sources["gene"].unique())
+a.columns = ["genes"]
+a["genes"].to_clipboard(index=False)
