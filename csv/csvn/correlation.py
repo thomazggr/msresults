19e0fe71184cb049a7b73f4f91d876d21724a624
Thomaz Guadagnini <thomaz@vivaldi.net>
2021-04-04 16:58:36 -0300

    moving files

--- csv/csvn/correlation.py
+++ csv/csvn/correlation.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-xp_val = pd.read_csv("csv/csvn/exp_val.csv", sep=",")
-
+xp_val = pd.read_csv("results_nashonly/exp-val.csv", sep=",")
+xp_val.set_index("ID_REF", inplace=True, drop=True)
+xp_val = xp_val.T
+# print(xp_val)
 xp_val.corr(method="pearson").to_clipboard()
+# print(xp_val.corr(method="pearson"))
