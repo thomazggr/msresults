95967e191c9b777e15bcb4a35c1553eee085680a
Thomaz Guadagnini <thzgr@tuta.io>
2022-07-31 16:05:11 -0300

    add roc results based on all 3 datasets itself

--- automated_results/padj005_logfc1/13_genes/AUC_ROCCurve_raw_data.R
+++ automated_results/padj005_logfc1/13_genes/AUC_ROCCurve_raw_data.R
@@ -28,18 +28,17 @@ get.annotation <- function(organism = NULL) {
     attributes_BM <- dplyr::rename(attributes_BM, 
                                     target_id = ensembl_transcript_id,ens_gene = ensembl_gene_id,
                                     ext_gene = external_gene_name, entrez_id = entrezgene_id,
-                                    illumina = affy_hg_u133_plus_2)
+                                    illumina = illumina_humanwg_6_v3, affy=affy_hg_u133_plus_2)
     attributes_BM <- dplyr::select(attributes_BM, 
-                                    c('ext_gene', 'entrez_id', 'illumina')) %>%
-                        dplyr::mutate(illumina = stringr::str_replace(illumina, "_s_at", ""),
-                                      entrez_id = as.character(entrez_id))
+                                    c('ext_gene', 'entrez_id', 'illumina', 'affy')) %>%
+                        dplyr::mutate(entrez_id = as.character(entrez_id))
     
     return(unique(attributes_BM))
 }
 
 Sys.setenv(VROOM_CONNECTION_SIZE = "500000")
 
-get_expression <- function(gse, annot_gpl, gpl, samples, annotation_data, gene_names_filter, labels){
+except_get_expression <- function(gse, annot_gpl, gpl, samples, annotation_data, gene_names_filter, labels){
     gset <- GEOquery::getGEO(gse, GSEMatrix =TRUE, AnnotGPL=annot_gpl)
     if (length(gset) > 1) idx <- grep(gpl, attr(gset, "names")) else idx <- 1
     gset <- gset[[idx]]
@@ -78,8 +77,8 @@ get_expression <- function(gse, annot_gpl, gpl, samples, annotation_data, gene_n
                             dplyr::distinct(ext_gene, .keep_all=TRUE)
 
     row.names(exp_data_filtered) <- exp_data_filtered$ext_gene
-    print(head(exp_data_filtered))
-    exp_data_filtered <- dplyr::select(exp_data_filtered, -c(illumina.x, illumina.y, entrez_id, ext_gene)) %>%
+    
+    exp_data_filtered <- dplyr::select(exp_data_filtered, -c(illumina.x, illumina.y, entrez_id, ext_gene, affy)) %>%
                             t() %>%
                             as.data.frame()
     
@@ -88,6 +87,49 @@ get_expression <- function(gse, annot_gpl, gpl, samples, annotation_data, gene_n
     return(exp_data_filtered)
 }
 
+common_get_expression <- function(gse, annot_gpl, gpl, samples, annotation_data, gene_names_filter, labels){
+    gset <- GEOquery::getGEO(gse, GSEMatrix =TRUE, AnnotGPL=annot_gpl)
+    if (length(gset) > 1) idx <- grep(gpl, attr(gset, "names")) else idx <- 1
+    gset <- gset[[idx]]
+
+    # make proper column names to match toptable 
+    fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+    # group membership for all samples
+    gsms <- samples
+    sml <- strsplit(gsms, split="")[[1]]
+
+    # filter out excluded samples (marked as "X")
+    sel <- which(sml != "X")
+    sml <- sml[sel]
+    gset <- gset[ ,sel]
+
+    # log2 transformation
+    ex <- exprs(gset)
+    qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+    LogC <- (qx[5] > 100) ||
+            (qx[6]-qx[1] > 50 && qx[2] > 0)
+    if (LogC) { ex[which(ex <= 0)] <- NaN
+    exprs(gset) <- log2(ex) }
+
+    expression_data <- as.data.frame(exprs(gset))
+    expression_data$illumina <- row.names(expression_data)
+
+    exp_data_att <- expression_data %>%
+                    dplyr::left_join(annotation_data, 
+                                by=c("illumina"))
+
+    exp_data_filtered <- dplyr::filter(exp_data_att, ext_gene %in% gene_names_filter) %>%
+                            dplyr::distinct(ext_gene, .keep_all=TRUE)
+
+    row.names(exp_data_filtered) <- exp_data_filtered$ext_gene
+    exp_data_filtered <- dplyr::select(exp_data_filtered, -c(illumina, entrez_id, ext_gene, affy)) %>%
+                            t() %>%
+                            as.data.frame()
+
+    exp_data_filtered$labels <- labels
+    return(exp_data_filtered)
+}
 #d <- read.csv("~/Projects/msresults/automated_results/GSE37031/GSE37031.tsv", sep="\t")
 
 #d %>% dplyr::filter(SPOT_ID %in% gene_entrez)
@@ -117,14 +159,14 @@ super_df <- data.frame(matrix(ncol = 14, nrow = 0))
 colnames(super_df) <- gene_names_filter
 
 datasets <- list(
-    # list("GSE33814", TRUE, "GPL6884", "1XXX1X111111111XX1XXXXXX000XX0XXX00000X00X00", 
-    #     c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-    #         TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-    #         FALSE, FALSE, FALSE, FALSE, FALSE, FALSE))
-    # list("GSE89632", FALSE, "GPL14951", "X1XXXXXX11X11XX111X1X111XXXX1X11111X10000000000000000X000X00000", 
-    #     c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-    #         TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-    #         FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE))
+    list("GSE33814", TRUE, "GPL6884", "1XXX1X111111111XX1XXXXXX000XX0XXX00000X00X00", 
+        c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
+            TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+            FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
+    list("GSE89632", FALSE, "GPL14951", "X1XXXXXX11X11XX111X1X111XXXX1X11111X10000000000000000X000X00000", 
+        c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
+            TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+            FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)),
     list("GSE37031", FALSE, "GPL14877", "111111110000000",
         c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE))
 )
@@ -132,16 +174,19 @@ datasets <- list(
 ann <- get.annotation(organism="hsa")
 
 for(d in datasets){
-    # print(d)
-    # quit()
-    data <- get_expression(d[[1]], d[[2]], d[[3]], d[[4]], ann, gene_names_filter, d[[5]])
+    if(d[[1]] == "GSE37031"){
+        data <- except_get_expression(d[[1]], d[[2]], d[[3]], d[[4]], ann, gene_names_filter, d[[5]])
+    } else {
+        data <- common_get_expression(d[[1]], d[[2]], d[[3]], d[[4]], ann, gene_names_filter, d[[5]])
+    }
+    print(as.character(d[[1]]))
+    print(head(data, 5))
+    print("")
     super_df <- dplyr::bind_rows(super_df, data)
 }
 
-print(head(super_df, 3))
-#quit()
 for(col in gene_names_filter){
-    if(col %in% names(super_df)){
+    if(col %in% names(super_df) && col != "labels"){
         curr_df <- na.omit(dplyr::select(super_df, c(col, "labels")))
         if(nrow(curr_df) == 0){
             print("")
@@ -156,7 +201,7 @@ for(col in gene_names_filter){
             
             print("")
             print(paste(auc@y.name, col))
-            print(auc@y.values)
+            print(as.character(auc@y.values))
             print("")
             # ROCR::plot(perf)
         }
